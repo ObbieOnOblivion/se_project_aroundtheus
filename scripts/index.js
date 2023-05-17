de3ecccf9b07712af08f0735945eef57ac08bde1
Obbie Omoruyi <obbie.omoruyi@gmail.com>
2023-05-17 10:53:53 -0500

    add javascript to oped modal

---
+++ scripts/index.js
@@ -0,0 +1,47 @@
+
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg", 
+    },
+
+    {
+        name: "Lake Louise",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg", 
+    },
+
+    {
+        name: "Bald Mountains",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg", 
+    },
+
+    {
+        name: "Latemar",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg", 
+    },
+
+    {
+        name: "Vanoise National Park",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg", 
+    },
+
+    {
+        name: "Lago di Braies",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg", 
+    }
+]
+
+const profileEditButton = document.querySelector("#profile__edit-button")
+const profileEditModal = document.querySelector("#modal")
+const modalCloseButton = document.querySelector("#modal__close-button")
+
+console.log(profileEditButton)
+
+profileEditButton.addEventListener("click", () => profileEditModal.classList.toggle("modal_open"))
+
+modalCloseButton.addEventListener("click", () => profileEditModal.classList.toggle("modal_open"))
+
+
+
+
+ 
